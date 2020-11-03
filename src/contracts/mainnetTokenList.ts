c705a7adba9951fb0487ce947fa1aa125929bdb9
althash-leandro <lmachado@htmlcoin.team>
2020-11-03 10:02:03 -0300

    v0.1.1 update colors and token

--- src/contracts/mainnetTokenList.ts
+++ src/contracts/mainnetTokenList.ts
@@ -1,6 +1,7 @@
 import QRCToken from '../models/QRCToken';
 
 const mainnetTokenList: QRCToken[] = [
+  new QRCToken('Sovereign Money', 'SVM', 8, '5250a3a8e052b76ef18675eb96503e77c32f8ac7'),
   new QRCToken('CODEX', 'CDEX', 8, '911106d88ae972bd06d68dc627c2997a6fe8205f'),
 ];
 
